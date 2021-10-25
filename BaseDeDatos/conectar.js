40d42531cede46e86e11f78fab1fa0f14a30abf0
Nicolas Narvaez <nkxg4130@gmail.com>
2021-10-24 22:48:34 -0500

    cambios

--- BaseDeDatos/conectar.js
+++ BaseDeDatos/conectar.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/baseDeDatos1');
+mongoose.connect('mongodb+srv://HelloWorldTeam:<password>@hwtbd.0sgoj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 
 module.exports = mongoose;
 
+// wMiEyoG2Ou2mr35U contraseña usuario mongo
